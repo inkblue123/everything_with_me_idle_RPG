014481bca3b06e039cb773b10c9860602c68a42a
inkblue <2944015870@qq.com>
2024-11-21 19:54:49 +0800

    继续开发主界面ui，加了两个按钮测试切换功能

--- hjj_work/everything_with_me_idle/js/Dom.js
+++ hjj_work/everything_with_me_idle/js/Dom.js
@@ -19,13 +19,13 @@ function addElement(parent_element, elem, id, cls) {
 }
 
 // 创建出所有需要的界面
-dom.player_attribute = crtElement("div", null, "section", "none", "#000000");
+dom.player_attribute = crtElement("div", null, "section", "", "#000000");
 dom.player_attribute.textContent = "角色属性界面";
 
 dom.player_equipment = crtElement("div", null, "section", "none", "#0000ff");
 dom.player_equipment.textContent = "角色装备界面";
 
-dom.backpack = crtElement("div", null, "section", "none", "#00ff00");
+dom.backpack = crtElement("div", null, "section", "", "#00ff00");
 dom.backpack.textContent = "背包物品界面";
 
 dom.skill = crtElement("div", null, "section", "none", "#ff0000");
@@ -34,65 +34,91 @@ dom.skill.textContent = "技能列表界面";
 dom.library = crtElement("div", null, "section", "none", "#00ffff");
 dom.library.textContent = "图鉴界面";
 
-dom.control = crtElement("div", null, "section", "none", "#ff00ff");
+dom.control = crtElement("div", null, "section", "", "#ff00ff");
 dom.control.textContent = "玩家控制界面";
 
-dom.create = crtElement("div", null, "section", "none", "#ffff00");
+dom.create = crtElement("div", null, "section", "", "#ffff00");
 dom.create.textContent = "合成制作界面";
 
-dom.map = crtElement("div", null, "section", "none", "#33fff9");
+dom.map = crtElement("div", null, "section", "", "#33fff9");
 dom.map.textContent = "地图界面";
 
-dom.Reserved = crtElement("div", null, "section", "none", "#3357ff");
-dom.Reserved.textContent = "占位界面";
+dom.remenber = crtElement("div", null, "section", "", "#3357ff");
+dom.remenber.textContent = "脑海界面";
 
-dom.combat_setting = crtElement("div", null, "section", "none", "#ff33a1");
+dom.combat_setting = crtElement("div", null, "section", "", "#ff33a1");
 dom.combat_setting.textContent = "战斗规划界面";
 
-dom.combat = crtElement("div", null, "section", "none", "#33fff9");
+dom.combat = crtElement("div", null, "section", "", "#33fff9");
 dom.combat.textContent = "战斗界面";
 
+dom.game_dom = crtElement("div", null, "main_page", "");
+
+dom.game_left = crtElement("div", null, "page_columns_1", "none");
+dom.game_right = crtElement("div", null, "page_columns_1", "none");
 //战斗时位于游戏上半部分的容器
-dom.game_up_combat = document.createElement("div");
-dom.game_up_combat.className = "page_2section";
-dom.game_up_combat.style.display = "none";
+dom.game_up_combat = crtElement("div", null, "page_columns_1", "none");
 //平时位于游戏上半部分的容器
-dom.game_up_nomal = document.createElement("div");
-dom.game_up_nomal.className = "page_3section";
-dom.game_up_nomal.style.display = "none";
+dom.game_up_nomal = crtElement("div", null, "page_columns_11", "none");
 //平时位于游戏上半部分的容器
-dom.game_down_nomal = document.createElement("div");
-dom.game_down_nomal.className = "page_3section";
-dom.game_down_nomal.style.display = "none";
+dom.game_down_nomal = crtElement("div", null, "page_columns_11", "none");
+
+dom.option_dom = crtElement("div", null, "option_page", "");
 
 // UI设计
-dom.main_dom = addElement(document.body, "div", null, "game_page");
-//给整体容器分两部分，其实上部分有两个，要根据具体情况分别显示
-dom.main_dom.appendChild(dom.game_up_nomal);
-// dom.game_up_nomal.style.display = "";
-dom.main_dom.appendChild(dom.game_up_combat);
-dom.game_up_combat.style.display = ""; //默认处于非战斗时刻，不显示
-dom.main_dom.appendChild(dom.game_down_nomal);
-dom.game_down_nomal.style.display = "";
+dom.main_dom = addElement(document.body, "div", null, "page_columns_1");
 
-//给每个部分填入具体的界面
-dom.game_up_combat.appendChild(dom.player_attribute);
-dom.player_attribute.style.display = "";
+dom.main_dom.appendChild(dom.game_dom);
+
+dom.game_dom.appendChild(dom.game_left);
+dom.game_dom.appendChild(dom.game_right);
+
+dom.game_left.appendChild(dom.player_attribute);
+dom.game_left.appendChild(dom.backpack);
+
+dom.game_right.appendChild(dom.game_up_combat);
+dom.game_right.appendChild(dom.game_up_nomal);
+dom.game_right.appendChild(dom.game_down_nomal);
+
 dom.game_up_combat.appendChild(dom.combat);
-dom.combat.style.display = "";
 
-dom.game_up_nomal.appendChild(dom.player_equipment); //并不能真的共用，要改
-dom.player_equipment.style.display = "";
-dom.game_up_nomal.appendChild(dom.Reserved);
-dom.Reserved.style.display = "";
+dom.game_up_nomal.appendChild(dom.remenber);
 dom.game_up_nomal.appendChild(dom.map);
-dom.map.style.display = "";
 
-dom.game_down_nomal.appendChild(dom.backpack);
-dom.backpack.style.display = "";
 dom.game_down_nomal.appendChild(dom.control);
-dom.control.style.display = "";
 dom.game_down_nomal.appendChild(dom.create);
-dom.create.style.display = "";
+dom.game_down_nomal.appendChild(dom.combat_setting);
 
-export { dom };
+dom.main_dom.appendChild(dom.option_dom);
+
+//向游戏布局中填充战斗时的默认界面
+dom.InitGameDomCombat = function () {
+    dom.main_dom.style.display = "";
+
+    dom.game_left.style.display = "";
+    dom.game_right.style.display = "";
+
+    dom.game_up_combat.style.display = "";
+    dom.game_up_nomal.style.display = "none";
+    dom.game_down_nomal.style.display = "";
+
+    dom.create.style.display = "none";
+    dom.combat_setting.style.display = "";
+};
+
+//向游戏布局中填充正常时的默认界面
+dom.InitGameDomNomal = function () {
+    dom.main_dom.style.display = "";
+
+    dom.game_left.style.display = "";
+    dom.game_right.style.display = "";
+
+    dom.game_up_combat.style.display = "none";
+    dom.game_up_nomal.style.display = "";
+    dom.game_down_nomal.style.display = "";
+
+    dom.create.style.display = "";
+    dom.combat_setting.style.display = "none";
+};
+
+export { dom, addElement };
--- hjj_work/everything_with_me_idle/js/main.js
+++ hjj_work/everything_with_me_idle/js/main.js
@@ -1,1 +1,15 @@
-import { dom } from "./Dom.js";
+import { dom, addElement } from "./Dom.js";
+
+window.addEventListener("load", () => {
+    dom.InitGameDomNomal();
+});
+var button1 = addElement(dom.option_dom, "button");
+button1.innerHTML = "战斗时的ui";
+button1.onclick = function () {
+    dom.InitGameDomCombat();
+};
+var button2 = addElement(dom.option_dom, "button");
+button2.innerHTML = "平时的ui";
+button2.onclick = function () {
+    dom.InitGameDomNomal();
+};
